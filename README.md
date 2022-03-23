4cb46104d3b545cf05516c56926991634ff7d6c8
marcossssantos1 <marcossantosmsms604@gmail.com>
2022-03-23 16:55:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sn6ptw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sn6ptw)
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useRef } from "react";
+import JoditEditor from "jodit-react";
+
+import './style.css'
+
+function App() {
+  const editor = useRef(null);
+  const [content, setContent] = useState("Start writing");
+
+  const config = {
+    readonly: false,
+    height: 400,
+  };
+  const handleUpdate = (event) => {
+    const editorContent = event.target.value;
+    setContent(editorContent);
+  };
+
+  return (
+    <div className="App">
+      <JoditEditor
+        ref={editor}
+        value={content}
+        config={config}
+        onBlur={handleUpdate}
+        onChange={(newContent) => {}}
+      />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+}
+
+export default App;
+
